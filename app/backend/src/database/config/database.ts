0d04b55a30574a32beb49f87aba6a1c43bca3381
Gabriela <gabrivicentini@icloud.com>
2024-06-04 20:39:44 -0300

    Config changes

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,16 +1,35 @@
 import { Options } from 'sequelize';
+import { parse } from 'pg-connection-string';
 
-const config: Options = {
-  username: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '123456',
-  database: process.env.MYSQLDATABASE || 'PLACING_FOOTBALL_CLUB',
-  host: process.env.MYSQLHOST || 'localhost',
-  port: process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306,
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
+let config: Options;
+
+if (process.env.MYSQL_URL) {
+  const params = parse(process.env.MYSQL_URL);
+  config = {
+    username: params.user || '',
+    password: params.password,
+    database: params.database || '',
+    host: params.host || '',
+    port: params.port ? parseInt(params.port) : undefined,
+    dialect: 'mysql',
+    dialectOptions: {
+      timezone: 'Z',
+    },
+    logging: false,
+  };
+} else {
+  config = {
+    username: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || '123456',
+    database: process.env.MYSQLDATABASE || 'PLACING_FOOTBALL_CLUB',
+    host: process.env.MYSQLHOST || 'localhost',
+    port: process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      timezone: 'Z',
+    },
+    logging: false,
+  };
 }
 
 export = config;
